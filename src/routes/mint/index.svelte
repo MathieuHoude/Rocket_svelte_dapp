c52f3c2ff49a6f56393fc150d8835542574c563a
MathieuHoude <mathieu.houde.6@gmail.com>
2022-09-29 20:28:23 -0400

    connecting to hydrawallet

--- src/routes/mint/index.svelte
+++ src/routes/mint/index.svelte
@@ -1,8 +1,25 @@
 <script lang="ts">
     import { Container, Image, Button } from 'sveltestrap';
     import Carousel from 'svelte-carousel'
-  
+    import { onMount } from 'svelte';
+
     const image_size = 400;
+
+    onMount(
+        window.postMessage({ message: { type: 'CONNECT_HYDRAWALLET' }}, '*')
+    )
+
+    function handleHydrawalletAcctChanged(event) {
+        if (event.data.message && event.data.message.type === "HYDRAWALLET_ACCOUNT_CHANGED") {
+                if (event.data.message.payload.error){
+                        // handle error
+                }
+        console.log("account:", event.data.message.payload.account)
+    } 
+}
+window.addEventListener('message', handleHydrawalletAcctChanged, false);
+
+
   </script>
 
   <style>
