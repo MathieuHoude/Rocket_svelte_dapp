f0989f7630b72bf2a6c2dce63243618274c95046
MathieuHoude <mathieu.houde.6@gmail.com>
2022-10-11 07:45:59 -0400

    test

--- src/routes/mint/index.svelte
+++ src/routes/mint/index.svelte
@@ -4,10 +4,22 @@
     import { onMount } from 'svelte';
     import { Hydraweb3 } from 'hydraweb3-js';
 
+    let hydraweb3, contract, account;
+
+    const contractAddress = '487a8137404198dd6bcc162fda4626e1cc032c22';
+    import HydraNFTABI from './HydraNFT.json';
+
+    let balance = null;
+    let minted = 'Please wait...'
+
+    // $: balance = isConnected() ? $web3.eth.getBalance(checkAccount) : ''
+
     const image_size = 400;
 
-    onMount(
-        window.postMessage({ message: { type: 'CONNECT_HYDRAWALLET' }}, '*')
+    onMount( async () => {
+            window.postMessage({ message: { type: 'CONNECT_HYDRAWALLET' }}, '*')
+            minted = await getTotalSupply();
+        }
     )
 
     function handleHydrawalletAcctChanged(event) {
@@ -16,8 +28,15 @@
                     // handle error
             }
             console.log("account:", event.data.message.payload.account)
-            const hydraweb3 = new Hydraweb3(window.hydrawallet.rpcProvider);
+            if (event.data.message.payload.account.loggedIn) {
+                account = event.data.message.payload.account;
+                balance = event.data.message.payload.account.balance / 100000000;
+            }
+
+            hydraweb3 = new Hydraweb3(window.hydrawallet.rpcProvider);
             console.log("hydraweb3: ", hydraweb3)
+            // Create a new Contract instance and use the same provider as hydraweb3
+            contract = hydraweb3.Contract(contractAddress, HydraNFTABI);
         } 
     }
     function handleHydrawalletInstalledOrUpdated(event) {
@@ -29,19 +48,33 @@
     window.addEventListener('message', handleHydrawalletInstalledOrUpdated, false);
     window.addEventListener('message', handleHydrawalletAcctChanged, false);
 
+    async function isConnected() {
+        return await hydraweb3.isConnected();
+    }
+
+    async function getTotalSupply() {
+        const data = hydraweb3.encoder.constructData(contract.abi, "totalSupply", []);
+        let result = await contract.provider.rawCall('callcontract', [contract.address, data, account]);
+        console.log("response: ", result)
+        let name = hydraweb3.decoder.decodeCall(result, contract.abi, "totalSupply", true);
+        console.log(name)
+    }
 
 </script>
 
 <style>
-    section, h1 {
+    section, h1, h2 {
         display: flex;
         justify-content: center;
     }
 </style>
 
 <Container fluid>
-    <h1>Mint your NFT</h1>
-    
+    <h1>Your current balance:</h1>
+    <h2>{balance}</h2>
+    <h1>Total NFT minted: </h1>
+    <h2>{minted}</h2>
+    <button on:click={getTotalSupply}></button>
 
     
 </Container>
